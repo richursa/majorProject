01282b15c21e6b8ef958ce488e4cbf79723e3498
richu <richursa@gmail.com>
2019-12-23 08:26:13 +0530

    added client stub for testing

--- node/api.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser')
-const api = require('./router/router.js')
-api.use(bodyParser.json());
-app.use('/api',api);
-app.listen(8080,()=>{
-    console.log('started listening on port 8080')
-});
